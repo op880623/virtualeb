09b301b1e155d4c176426df1b8150b8e0c92c9ee
op880623 <op880623@gmail.com>
2017-05-09 21:51:07 +0800

    url redirect modified.

--- article/urls.py
+++ article/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.list_all, name='url_list'),
-    url(r'^(?P<id>[0-9]+)/$', views.article_redirect, name='url_article_redirect'),
-    url(r'^(?:(?P<category>\S+)/)?(?P<id>[0-9]+)/(?:(?P<slug>\S+)/)?$', views.article, name='url_article'),
-    url(r'^(?P<category>.+)$', views.list, name='url_list'),
-    url(r'^(?P<category>.+)$', views.list_redirect, name='url_list_redirect'),
+    url(r'^(?P<category>\S+/)?(?P<id>[0-9]+)/(?:(?P<slug>\S+)/)?$', views.article, name='url_article'),
+    url(r'^(?P<category>\S+/)$', views.list, name='url_list'),
 ]
--- article/views.py
+++ article/views.py
@@ -6,9 +6,6 @@ from .forms import CommentForm
 import re
 
 
-def list_redirect(request, category):
-    return redirect(reverse('url_list', kwargs={'category': category}))
-
 def list(request, category):
     classification = get_object_or_404(Classification, name__iregex=re.sub('-', '.', '^'+category.split('/')[-2]+'$'))
     if classification.format_url() == category:
@@ -19,11 +16,10 @@ def list(request, category):
             articles = paginator.page(page)
         except:
             articles = paginator.page(1)
-        children = classification.list_child()
         categories = get_list_or_404(Classification)
-        return render(request, 'startbootstrap-blog-4-dev/list.html', {'list_name': classification.name, 'children': children, 'articles': articles, 'categories': categories})
+        return render(request, 'startbootstrap-blog-4-dev/list.html', {'list_name': classification.name, 'articles': articles, 'categories': categories})
     else:
-        return redirect(reverse('url_list_redirect', kwargs={'category': classification.format_url()}))
+        return redirect(reverse('url_list', kwargs={'category': classification.format_url()}))
 
 def list_all(request):
     articles = Article.objects.all()
@@ -36,13 +32,9 @@ def list_all(request):
     categories = get_list_or_404(Classification)
     return render(request, 'startbootstrap-blog-4-dev/list.html', {'list_name': 'All articles', 'articles': articles, 'categories': categories})
 
-def article_redirect(request, id):
-    article = get_object_or_404(Article, id=int(id))
-    return redirect(reverse('url_article', kwargs={'id': str(int(id)).zfill(5), 'slug': article.slug(), 'category': article.classification.format_url()[:-1]}))
-
 def article(request, id, slug='', category=''):
     article = get_object_or_404(Article, id=int(id))
-    if slug == article.slug() and id == str(int(id)).zfill(5) and category == article.classification.format_url()[:-1]:
+    if slug == article.slug() and id == str(int(id)).zfill(5) and category == article.classification.format_url():
         if request.method == 'POST':
             if CommentForm(request.POST).is_valid():
                 form = CommentForm(request.POST)
@@ -54,4 +46,4 @@ def article(request, id, slug='', category=''):
         categories = get_list_or_404(Classification)
         return render(request, 'startbootstrap-blog-4-dev/article.html',  {'article': article, 'form': form, 'comments': comments, 'categories': categories})
     else:
-        return redirect(reverse('url_article_redirect', kwargs={'id': str(int(id)).zfill(5)}))
+        return redirect(reverse('url_article', kwargs={'id': str(int(id)).zfill(5), 'slug': article.slug(), 'category': article.classification.format_url()}))
